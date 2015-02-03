bbf34e7089693d97d7b6c2755353f6e32dafbc64
Diego Mascialino <dmascialino@gmail.com>
2015-02-03 14:44:38 -0300

    fix #42, configure each circuit and iterate for ever

--- firmware.c
+++ firmware.c
@@ -7,21 +7,65 @@
 #include "firmware.h"
 #include "measurements.h"
 
+
+#define N_CIRCUITS 6
+
+circuit_t CIRCUITS[] = {
+		{ .circuit_id = 1,
+		  .V_adc = &ADCA, .V_pin = ADCCH_POS_PIN0,
+		  .I_adc = &ADCA, .I_pin = ADCCH_POS_PIN6,
+		  .V_dc_offset = 0,
+		  .V_gain = 1, .I_gain = 1,
+		  .delay = 100
+		},
+		{ .circuit_id = 2,
+		  .V_adc = &ADCA, .V_pin = ADCCH_POS_PIN1,
+		  .I_adc = &ADCA, .I_pin = ADCCH_POS_PIN7,
+		  .V_dc_offset = 0,
+		  .V_gain = 1, .I_gain = 1,
+		  .delay = 100
+		},
+		{ .circuit_id = 3,
+		  .V_adc = &ADCA, .V_pin = ADCCH_POS_PIN2,
+		  .I_adc = &ADCB, .I_pin = ADCCH_POS_PIN0,
+		  .V_dc_offset = 0,
+		  .V_gain = 1, .I_gain = 1,
+		  .delay = 100
+		},
+		{ .circuit_id = 4,
+		  .V_adc = &ADCA, .V_pin = ADCCH_POS_PIN3,
+		  .I_adc = &ADCB, .I_pin = ADCCH_POS_PIN1,
+		  .V_dc_offset = 0,
+		  .V_gain = 1, .I_gain = 1,
+		  .delay = 100
+		},
+		{ .circuit_id = 5,
+		  .V_adc = &ADCA, .V_pin = ADCCH_POS_PIN4,
+		  .I_adc = &ADCB, .I_pin = ADCCH_POS_PIN2,
+		  .V_dc_offset = 0,
+		  .V_gain = 1, .I_gain = 1,
+		  .delay = 100
+		},
+		{ .circuit_id = 6,
+		  .V_adc = &ADCA, .V_pin = ADCCH_POS_PIN5,
+		  .I_adc = &ADCB, .I_pin = ADCCH_POS_PIN3,
+		  .V_dc_offset = 0,
+		  .V_gain = 1, .I_gain = 1,
+		  .delay = 100
+		},
+};
+
 int main(void)
 {
-	circuit_t circuit;
+	uint8_t circuit_idx = 0;
+
 	board_init();
 	sysclk_init();
 	communication_init();
 	time_init();
 
-	circuit.circuit_id = 1;
-	circuit.delay = 1000;
-	circuit.V_gain = 1;
-	circuit.I_gain = 1;
-	circuit.V_dc_offset = 0;
-
 	while (true) {
-	    measure(&circuit);
+		measure(CIRCUITS + circuit_idx);
+		circuit_idx = (circuit_idx + 1) % N_CIRCUITS;
 	}
 }

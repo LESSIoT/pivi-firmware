beabadb924b4e40f7cee44e9393f1d719373909a
Diego Mascialino <dmascialino@gmail.com>
2015-02-06 11:11:32 -0300

    cleaning measurments code

--- measurements.c
+++ measurements.c
@@ -4,11 +4,7 @@
 #include "firmware.h"
 #include "analog.h"
 #include "time.h"
-
-#define N_SAMPLES_BY_PERIOD 20
-#define I_SAMPLES_BUFF_SIZE (N_SAMPLES_BY_PERIOD / 1)
-#define N_PERIODS 1
-#define N_SAMPLES (N_PERIODS * N_SAMPLES_BY_PERIOD)
+#include "asf.h"
 
 
 volatile float I_samples_buffer[I_SAMPLES_BUFF_SIZE];
@@ -26,62 +22,62 @@ void measure_I_sample(void);
 
 void measure_V_sample(void)
 {
-	float V_sample = analog_get_V_sample();
-	uint16_t idx = V_samples_count % I_SAMPLES_BUFF_SIZE;
+    float V_sample = analog_get_V_sample();
+    uint16_t idx = V_samples_count % I_SAMPLES_BUFF_SIZE;
 
-	V_rms_acc += V_sample * V_sample;
-	power_acc += V_sample * I_samples_buffer[idx];
+    V_rms_acc += V_sample * V_sample;
+    power_acc += V_sample * I_samples_buffer[idx];
 
-	if (!V_samples_count)
-	{
-		time_set_V_period();
-	}
+    if (!V_samples_count)
+    {
+        time_set_V_period();
+    }
 
-	if (++V_samples_count == N_SAMPLES)
-	{
-		time_stop_V_timer();
-		measuring = false;
-	}
+    if (++V_samples_count == N_SAMPLES)
+    {
+        time_stop_V_timer();
+        measuring = false;
+    }
 }
 
 void measure_I_sample(void)
 {
-	uint16_t idx = I_samples_count % I_SAMPLES_BUFF_SIZE;
+    uint16_t idx = I_samples_count % I_SAMPLES_BUFF_SIZE;
 
-	I_samples_buffer[idx] = analog_get_I_sample();
-	I_rms_acc += I_samples_buffer[idx] * I_samples_buffer[idx];
+    I_samples_buffer[idx] = analog_get_I_sample();
+    I_rms_acc += I_samples_buffer[idx] * I_samples_buffer[idx];
 
-	if (++I_samples_count == N_SAMPLES)
-	{
-		time_stop_I_timer();
-	}
+    if (++I_samples_count == N_SAMPLES)
+    {
+        time_stop_I_timer();
+    }
 }
 
 void measure(circuit_t *circuit)
 {
-	measurement_packet_t packet;
+    measurement_packet_t packet;
 
-	I_samples_count = 0; V_samples_count = 0;
-	I_rms_acc = 0; V_rms_acc = 0; power_acc = 0;
+    I_samples_count = 0; V_samples_count = 0;
+    I_rms_acc = 0; V_rms_acc = 0; power_acc = 0;
 
 
-	analog_config(circuit);
+    analog_config(circuit);
 
-	measure_I_sample();
-	time_start_timers(circuit->delay);
-	time_set_V_callback(measure_V_sample);
-	time_set_I_callback(measure_I_sample);
+    measure_I_sample();
+    time_start_timers(circuit->delay);
+    time_set_V_callback(measure_V_sample);
+    time_set_I_callback(measure_I_sample);
 
-	measuring = true;
+    measuring = true;
 
-	/* wait until all the samples are taken */
-	while (measuring);
+    /* wait until all the samples are taken */
+    while (measuring);
 
-	packet.circuit_id = circuit->circuit_id;
+    packet.circuit_id = circuit->circuit_id;
 
-	packet.real_power = (power_acc / N_SAMPLES);
-	packet.irms = (I_rms_acc / N_SAMPLES);
-	packet.vrms = (V_rms_acc / N_SAMPLES);
+    packet.real_power = (power_acc / N_SAMPLES);
+    packet.irms = (I_rms_acc / N_SAMPLES);
+    packet.vrms = (V_rms_acc / N_SAMPLES);
 
-	send_to_pi(&packet);
+    send_to_pi(&packet);
 }
--- measurements.h
+++ measurements.h
@@ -2,9 +2,13 @@
 #ifndef _MEASUREMENTS_H
 #define _MEASUREMENTS_H
 
-#include "asf.h"
 #include "firmware.h"
 
 void measure(circuit_t *circuit);
+
+#define N_SAMPLES_BY_PERIOD 20
+#define I_SAMPLES_BUFF_SIZE (N_SAMPLES_BY_PERIOD / 1)
+#define N_PERIODS 1
+#define N_SAMPLES (N_PERIODS * N_SAMPLES_BY_PERIOD)
 
 #endif // _MEASUREMENTS_H

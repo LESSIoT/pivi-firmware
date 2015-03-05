aa0379659a821850e2dfcf230c9d4409758953bf
Lucas Chiesa <lucas.chiesa@gmail.com>
2015-03-05 16:41:22 -0300

    Add License file and biolerplate test

--- measurements.c
+++ measurements.c
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the Pivi firmware package.
+ * Released under the Creative Commons ATT-NC-ShareAlire license
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ * Copyright (C) 2014, 2015 LESS Industries S.A.
+ * Author: Diego Mascialino <dmascialino@gmail.com>
+ */
+
 
 #include "measurements.h"
 #include "communication.h"

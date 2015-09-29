8fedc53b73dd7a1cc58e1a9058c0f166965a8a00
nachocarballeda <carballeda.ignacio@gmail.com>
2015-09-29 19:03:03 -0300

    Agrego pivy_1 con valores de ganancia puestos a mano, queda ver porque la potencia da mal

---
+++ board_calibration_data/pivy_1.h
@@ -0,0 +1,48 @@
+
+
+#define C1_V_OFFSET 2707
+#define C1_I_OFFSET 2699
+#define C1_V_GAIN 647 
+#define C1_I_GAIN 120
+#define C1_DELAY 400
+
+
+
+#define C2_V_OFFSET 2703
+#define C2_I_OFFSET 2692
+#define C2_V_GAIN 647
+#define C2_I_GAIN 1
+#define C2_DELAY 400
+
+
+
+#define C3_V_OFFSET 2703
+#define C3_I_OFFSET 2696
+#define C3_V_GAIN 647
+#define C3_I_GAIN 1
+#define C3_DELAY 400
+
+
+
+#define C4_V_OFFSET 2701
+#define C4_I_OFFSET 2691
+#define C4_V_GAIN 647
+#define C4_I_GAIN 1
+#define C4_DELAY 400
+
+
+
+#define C5_V_OFFSET 2706
+#define C5_I_OFFSET 2703
+#define C5_V_GAIN 647
+#define C5_I_GAIN 1
+#define C5_DELAY 400
+
+
+
+#define C6_V_OFFSET 2702
+#define C6_I_OFFSET 2692
+#define C6_V_GAIN 647
+#define C6_I_GAIN 1
+#define C6_DELAY 400
+
